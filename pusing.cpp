43d445a6c45a3feff8a9ccb279849db50d2e9830
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-03-11 11:55:19 +0700

    tambah prosedur masukanData

--- pusing.cpp
+++ pusing.cpp
@@ -2,6 +2,13 @@
 #include <string>
 using namespace std;
 
+void masukanData(float &berat, float &tinggi) {
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
+
 int main() {
     return 0;
 }
