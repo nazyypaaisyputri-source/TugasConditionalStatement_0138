161f962bb55b8f65e5f85a03fce02037940fa72f
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-03-11 12:00:56 +0700

    tambah variabel dan input data pada main

--- pusing.cpp
+++ pusing.cpp
@@ -13,6 +13,16 @@ float hitungBMI(float berat, float tinggi) {
     return berat / (tinggi * tinggi);
 }
 
+string cekKondisi(float bmi) {
+    if (bmi < 18.5) return "Berat Badan Kurang";
+    else if (bmi < 25) return "Berat Badan Normal";
+    else if (bmi < 30) return "Berat Badan Kelebihan";
+    else return "Obesitas";
+}
+
 int main() {
+    float berat, tinggi, hasilBMI;
+    cout << "--- Selamat Datang Mahasiswa Kelas A 2025 ---" << endl;
+    masukanData(berat, tinggi);
     return 0;
 }
